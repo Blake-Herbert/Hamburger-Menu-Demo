83454dcda284d8d439f7c5b916830a25137269ee
Blake Herbert <Bwherb1@gmail.com>
2024-11-12 14:07:03 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle Hamburger menu display on click
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const headerLinks = document.querySelector('.header-links');
+
+    hamburgerMenu.addEventListener('click', function () {
+        headerLinks.classList.toggle('show');
+    });
+});
